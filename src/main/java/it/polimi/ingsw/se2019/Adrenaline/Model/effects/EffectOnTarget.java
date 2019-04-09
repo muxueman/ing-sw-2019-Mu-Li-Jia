f0a774a88b599d2d4323176cc8f3d8ce6feda1d5
greta <gretajia@foxmail.com>
2019-04-09 15:49:13 +0200

    CHANGE EFFECT CLASS

--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/Model/effects/EffectOnTarget.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/Model/effects/EffectOnTarget.java
@@ -1,28 +1,20 @@
 package it.polimi.ingsw.se2019.Adrenaline.Model.effects;
-
 import it.polimi.ingsw.se2019.Adrenaline.Model.Player;
 
-/**
- * 
+import java.util.*;
+
+/**@author Xueman Mu
+ * @author Moxin Jia
+ * @author Xuejing Li
  */
 public class EffectOnTarget extends Effect {
 
-    /**
-     * Default constructor
-     */
     public EffectOnTarget() {
     }
 
-    /**
-     * 
-     */
     private int damageNum;
-
-    /**
-     * 
-     */
     private int markDamageNum;
-
+    
     public void effect(Player player) {
         // TODO implement here
     }
--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/Model/effects/TargetBackEffect.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/Model/effects/TargetBackEffect.java
@@ -1,43 +1,26 @@
 package it.polimi.ingsw.se2019.Adrenaline.Model.effects;
 import it.polimi.ingsw.se2019.Adrenaline.Model.Player;
+import it.polimi.it.se2019.Model.Effect;
 
+import java.util.*;
 
-/**
- * 
+/**@author Xueman Mu
+ * @author Moxin Jia
+ * @author Xuejing Li
  */
-public class TargetBackEffect extends Effect {
+public class targetBackEffect extends Effect {
 
-    /**
-     * Default constructor
-     */
-    public TargetBackEffect() {
+    public targetBackEffect() {
     }
 
-    /**
-     * 
-     */
     private int stepBackNum;
-
-    /**
-     * 
-     */
     private int targetMove;
-
-    /**
-     * 
-     */
     private int targetMoveRange;
 
-
-    public ArrayList<Player> availbableTarget(Player player) {
-        // TODO implement here
-        return null;
+    public ArrayList<it.polimi.it.se2019.Model.Player> availbableTarget(it.polimi.it.se2019.Model.Player player) {
     }
 
-
-    public void effect(Player player) {
-        // TODO implement here
-
+    public void effect(it.polimi.it.se2019.Model.Player player) {
     }
 
 }
