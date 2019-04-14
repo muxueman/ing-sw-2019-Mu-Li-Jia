f5036ecc079159299084172e31b38ef5e1126bf2
Stella <muxuemanstella@hotmail.com>
2019-04-14 16:03:55 +0200

    hand over test report

---
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/Model/weaponCards/Ondadurto.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.se2019.Adrenaline.Model.weaponCards;
+import java.util.*;
+
+
+public class Ondadurto extends WeaponCard {
+
+    /**
+     * Default constructor
+     */
+    public Ondadurto() {
+    }
+
+}
