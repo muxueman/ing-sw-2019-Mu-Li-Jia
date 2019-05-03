a7bfb5247ba4e07048b2f35e90bbef55b3994289
Stella <muxuemanstella@hotmail.com>
2019-05-03 16:57:13 +0200

    update

--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/Controller/Controller.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/Controller/Controller.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw.se2019.Adrenaline.Controller;
+package it.polimi.ingsw.se2019.Adrenaline.server.controller;
 
 import java.util.*;
 //import it.polimi.ingsw.se2019.Adrenaline.View.View;
@@ -10,45 +10,3 @@ import it.polimi.ingsw.se2019.Adrenaline.server.model.KillShootTrack;
  * @author Moxin Jia
  * @author Xuejing Li
  */
-
-public class Controller {
-
-    public Controller() {
-    }
-
-    //private View view;
-
-    private PlayBoard playBoard;
-
-
-    public void updateAllOnBoard() {
-    }
-    public void updatePlayer(Player player) {
-
-    }
-
-    public void updateKillShootTrack(KillShootTrack killShootTrack) {
-    }
-
-    public void updatePlayboard(PlayBoard playboard) {
-    }
-    public void visitPlayBoardSelect(String message) {
-    }
-
-    public void visitActionSelect(String message) {
-    }
-
-    public void actFinalFrenzy() {
-    }
-    public ArrayList<Player> visitTargetSelect(String message) {
-        return null;
-    }
-    public void updateKillshoot() {
-    }
-
-    public void chooseMap() {
-    }
-    public void chooseStartPoint() {
-    }
-
-}
--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/Controller/cardController.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/Controller/cardController.java
@@ -1,29 +1,9 @@
-package it.polimi.ingsw.se2019.Adrenaline.Controller;
+package it.polimi.ingsw.se2019.Adrenaline.server.controller;
 
-import java.util.*;
 
 /**@author Xueman Mu
  * @author Moxin Jia
  * @author Xuejing Li
- */
-public class CardController {
+*/
 
 
-    public CardController() {
-    }
-
-    public void getTargetKillShoot() {
-    }
-
-
-    public void changeWeaponState() {
-    }
-
-    public void changePowerCardState() {
-    }
-
-    public void changeAmmotileState() {
-    }
-
-
-}
