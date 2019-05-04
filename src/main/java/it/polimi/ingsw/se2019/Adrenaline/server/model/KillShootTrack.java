351562984847cbaba617c0322b905e41a999000f
greta <gretajia@foxmail.com>
2019-05-04 13:28:40 +0200

    *

--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/model/KillShootTrack.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/model/KillShootTrack.java
@@ -1,72 +1,171 @@
 package it.polimi.ingsw.se2019.Adrenaline.server.model;
 
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
 /**
  * 
  */
-public class KillShootTrack  {
-
-    /**
-     * Default constructor
-     */
-    public KillShootTrack() {
-    }
+public class KillShootTrack implements Cloneable {
 
 
     private Player player;
-
-    /**
-     * 
-     */
     private int numKillShoot;
+    private ArrayList<Color> damageColorOnTrack;
+    private ArrayList<Color> markColorOnTrack;
+    private int turn;
+    private static int[] scoreTable = {8,6,4,2,2,1};
+    private static int maxiDamageOnTrack = 12;
 
-    /**
-     * 
-     */
-    private int currentKillShootTrack;
+    public  KillShootTrack() {
+        this.numKillShoot = 0;
+        turn = 0;
+        this.damageColorOnTrack = new ArrayList<Color>();
+        this.markColorOnTrack = new ArrayList<Color>();
+        //this.skull = new boolean[6];
+    }
+    //检查是否已有该玩家的Mark
+    public void addMarkToDamage(Player shooter){
+        int index = 0;
+        int markNum = 0;
+        while(index < markColorOnTrack.size()){
+            if(shooter.playerColor == markColorOnTrack.get((index))){
+                markNum++;
+                markColorOnTrack.remove(index);
+                damageColorOnTrack.add(shooter.playerColor);
+                index--;
+            }
+            index++;
+        }
+    }
+    //先增加原有的mark到damage， 增加新的damage， 增加新的Mark， 检查是否已经被打死，若打死的话 先清除多余的damage，检查是否被overkill, 超杀给shooter加mark
+    public void beAttacked (Player shooter, int damageNum, int markNum ) {
+        addMarkToDamage(shooter);
+        while(damageNum >0) {
+            this.damageColorOnTrack.add(shooter.playerColor);
+            damageNum--;
+        }
+        while(markNum >0) {
+            this.markColorOnTrack.add(shooter.playerColor);
+            damageNum--;
+        }
+        if(damageColorOnTrack.size() >= 12) {//如果被超杀死，先清除多余的
+            int i = maxiDamageOnTrack;
+            while (damageColorOnTrack.get(i) != null) {
+                damageColorOnTrack.remove(i);
+                i++;
+            }
+            killShoot();
+            overkillMark(shooter);
+        }
+        else if(damageColorOnTrack.size() == 11) {
+            killShoot();
+        }
+        else{
+            System.out.println("still alive");
+        }
+    }
+    public void overkillMark(Player shooter){
+        KillShootTrack shooterKillShootTrack = shooter.getKillShootTrack();
+        shooterKillShootTrack.markColorOnTrack.add(shooter.playerColor);
+    }
+    //count damage num of each color on the track, then add score.
+    public Map countDamageOnTrack() {
+        Map damageCount = new HashMap<Color, Integer>();
+        ArrayList<Color> copyColorOnTrack = (ArrayList<Color>)this.damageColorOnTrack.clone();
+        for(int i = 0; i < copyColorOnTrack.size()-1; i++){
+            Color currentColor = copyColorOnTrack.get(i);
+            int sum = 0;
+            for(int j = i+1; j < copyColorOnTrack.size(); j++){
+                Color comparedColor = copyColorOnTrack.get(j);
+                if(currentColor == comparedColor){
+                    sum++;
+                    damageCount.put(currentColor, sum);
+                    copyColorOnTrack.remove(j);
+                    j--;
+                }
+                else continue;
+            }
+        }
+        return damageCount;
+    }
+    public Map valueOfMapDownSort(Map inputMap) {
+        List<Map.Entry<String, Integer>> infoIds = new ArrayList<Map.Entry<String, Integer>>(inputMap.entrySet());
+        Collections.sort(infoIds, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return (o2.getValue() - o1.getValue());
+                //return (o1.getKey()).toString().compareTo(o2.getKey());
+            }
+        });
+        return inputMap;
+    }
 
+    public ArrayList<Integer> checkHighestScore(){
+        ArrayList<Integer> scoreArray = new ArrayList<Integer>();
+        int index = 0;
+        while((index + turn) < 6){
+            scoreArray.add(scoreTable[index +turn]);
+            index++;
+        }
+        return scoreArray;
+    }
 
+    public Map countPlayerScore() {
+        Map damageCount = countDamageOnTrack();
+        damageCount = valueOfMapDownSort(damageCount);
+        Map playerScore = new HashMap<Color, Integer>();
+        int i = 0;
+        ArrayList<Integer> scoreArray = checkHighestScore();
+        for (Object key : damageCount.keySet()){
+            playerScore.put(key, scoreArray.get(i));
+            i++;
+        }
+        return playerScore;
+    }
 
-
-
-    public void initialKillShootTrack(Player player, int numKillShoot) {
-        // TODO implement here
-
+    public Map scoreForfirstShooter(Map playerScore){
+        int sum = (int)(playerScore.get(this.damageColorOnTrack.get(0)));
+        sum++;
+        playerScore.put(this.damageColorOnTrack.get(0),sum);
+        return playerScore;
+    }
+    /*
+    public void addScoreToPlayer(ArrayList<Player> players){
+        Map playerScore = new HashMap(scoreForfirstShooter(countPlayerScore()));
+        int index = 0;
+        while(index < players.size()){
+            players.get(index).playerColor =
+        }
+    }
+    */
+    //被射杀，计算damage数，排序，查看最高分数（第几次被射杀），增加玩家的分数，第一射手得得分加1, 清理板子
+    public void killShoot(){
+        scoreForfirstShooter(countPlayerScore());
+        clearkillShootTrack();
+    }
+    public int getPlayerScore(Player player){
+        return player.getMyscore();
     }
 
 
-    public void triggerFrenzy() {
-        // TODO implement here
-
+    public ArrayList<Color> getDamageColorOnTrack(){
+        return damageColorOnTrack;
     }
 
-
-    public void killShoot() {
-        // TODO implement here
-
+    public void clearkillShootTrack(){
+        damageColorOnTrack.clear();
     }
-
-
-    public void overkillShoot() {
-        // TODO implement here
-
-    }
-
-
-    public void countEachTrackScore() {
-        // TODO implement here
-
-    }
-
-
-    public void nextKillShootTrack() {
-        // TODO implement here
-
-    }
-
-
-    public void countTotalTrackScore() {
-        // TODO implement here
-
+    public Object clone() {
+        Object o = null;
+        try {
+            o = super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return o;
     }
 
 }
--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/model/Player.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/model/Player.java
@@ -1,87 +1,115 @@
 package it.polimi.ingsw.se2019.Adrenaline.server.model;
 
 
-import it.polimi.ingsw.se2019.Adrenaline.Model.powerupCards.PowerupCard;
-import it.polimi.ingsw.se2019.Adrenaline.Model.weaponCards.WeaponCard;
-
 import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Player  {
-
-
-    private Player() {
-    }
-
-    private Color color;
-
+    protected int playerID;
+    protected Color playerColor;
     private String nickName;
-
-    private ArrayList<WeaponCard> weapons;
-
-
-    private ArrayList<PowerupCard> powerups;
-
-
-
-
-
-
-    private int totalscores;
-
-
-    private Cell currentRoom;
-
-
+    //private HashMap<WeaponCard, Boolean> weaponsOwned;
+    private int[] ammoOwned;
+    //private ArrayList<PowerupCard, Boolean> powerupsOwned;
+    //protected Player allPlayer[];
+    private int myScore;
+    private Cell currentCell;
     private KillShootTrack killShootTrack;
-
-
     private int actionMode;
+    /*mode = 0 three walk/one walk+ pick/shoot
+    // mode = 1  two walk+pick
+    mode = 1 walk+shoot*/
 
-
-    public void initialPlayer() {
-        // TODO implement here
-
-}
-
-
-
-
-
-    public int readscores() {
-        // TODO implement here
-        return 0;
+    //constructor
+    private Player(Color color) {
+        playerID = (int)(System.currentTimeMillis()%1000); //randomGenerated number
+        playerColor = color;
+        this.killShootTrack = new KillShootTrack();
+        actionMode = 0;
+        myScore = 0;
+        ammoOwned = new int[] {3,3,3};//RED, BLUE, YELLOW
+        //初始化所有的玩家,放入allPlayer
     }
 
-
-    public void addscores(int point) {
-        // TODO implement here
-
+    /*
+    //装载ammo
+    public void fillAmmo(ammotileCard){
+        ammoOwned.add(ammotileCard.ammo, )//color, num
+    }
+    //使用武器卡，消耗ammo
+    public void consumeAmmo(AmmoColor color, int num){
+        weaponsOwned.put(weaponCard, false);
+        int ammoCost
+        ammoOwned.put();
+    }
+    public void loadAmmo(AmmoColor color, int num){
     }
 
+    //
+    */
+    /*
+    public Player findPlayerByColor (Color playerColor){
+        int i = 0;
 
+        while(getAllPlayer([i].playerColor != playerColor){
+            i++;
+            if(i == allPlayer.length){
+                System.out.println("error: not exist");
+                break;
+            }
+        }
+        return allPlayer[i];
+    }
+    */
+    public void showActionMode(){
+        switch(actionMode){
+            case 0:
+                System.out.println("basic mode 0: three walk/one walk and pick/shoot ");
+            case 1:
+                System.out.println("basic mode 1: mode 0 upgrade to 'two walk and pick'");
+            case 2:
+                System.out.println("basic mode 2: mode 1 upgrade to 'one walk and shoot'");
+            case 3:
+                System.out.println("frenzy mode 1: one walk reload shoot/ four walk/ two walk pick");
+            case 4:
+                System.out.println("frenzy mode 2: two walk reload shoot/ three walk pick");
+        }
+    }
+    /*
+    public void countMyScore() {
+        int index = 0;
+        while(index < allPlayer.length){
+            myScore += (int)(allPlayer[index].killShootTrack.playerScore.get(playerColor));
+            index++;
+        }
+    }
+    */
+    /*
     public Cell getCurrentCell() {
-        // TODO implement here
-        return null;
     }
+    */
 
-    /**
-     * @return
-     */
-    public KillShootTrack getKillShootTrack() {
-        // TODO implement here
-        return null;
-    }
+    public void changeActionMode(){
+       int damageNum = this.killShootTrack.getDamageColorOnTrack().size();
 
-
-    public int getActionMode(KillShootTrack track) {
-        // TODO implement here
-        return 0;
+       //if(triggerfirenzy) this.actionMode = 3
+       if (damageNum > 2 & damageNum<= 5)
+           this.actionMode = 1;
+       else if(damageNum > 5 & damageNum <= 10)
+           this.actionMode = 2;
     }
 
 
-    public void takeAction() {
-        // TODO implement here
+    public KillShootTrack getKillShootTrack(){
+        return this.killShootTrack;
+    }
+    public void takeAction(int actionType) {
 
+    }
+    public int getMyscore(){
+        return myScore;
     }
 
 }
