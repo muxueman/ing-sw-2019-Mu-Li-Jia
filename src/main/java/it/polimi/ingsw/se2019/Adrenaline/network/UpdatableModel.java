cc9e954a4f24a1329d2da7adac0b242286b0bd6e
greta <gretajia@foxmail.com>
2019-06-28 19:16:55 +0200

    jia

--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/network/UpdatableModel.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/network/UpdatableModel.java
@@ -4,11 +4,11 @@ package it.polimi.ingsw.se2019.Adrenaline.network;
 import it.polimi.ingsw.se2019.Adrenaline.server.model.Player;
 import it.polimi.ingsw.se2019.Adrenaline.server.model.map.Map;
 import it.polimi.ingsw.se2019.Adrenaline.utils.immutables.BoardStatus;
+import it.polimi.ingsw.se2019.Adrenaline.utils.immutables.MapStatus;
 import it.polimi.ingsw.se2019.Adrenaline.utils.immutables.PlayerStatus;
 
 public interface UpdatableModel {
     void pingUpdate(String message);
-    void updateMap(Map selectedMap);
     void updatePlayboard(BoardStatus boardStatus);
     void updatePlayer(PlayerStatus playerStatus);
 
