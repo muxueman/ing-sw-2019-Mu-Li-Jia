cedaebc6c406e3ee46667df44f77964df0c402ab
greta <gretajia@foxmail.com>
2019-07-02 13:13:41 +0200

    jia

--- src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/model/action/ActionRun.java
+++ src/main/java/it/polimi/ingsw/se2019/Adrenaline/server/model/action/ActionRun.java
@@ -13,16 +13,27 @@ import java.io.Serializable;
 public class ActionRun implements Serializable {
     // If the player cant reach the cell he wanted to go, this function will return false, then the player have to decide
     // direction to run
-    public boolean ActionRun (Player player, int direction) throws InvalidRunException {
+    Player player;
+    public ActionRun(Player player) {
+        this.player = player;
+    }
+    public boolean ActionRun (int direction) throws InvalidRunException {
         boolean runSucceed = true;
         Cell nextcell = player.getCurrentCell().getNextCell(direction);
         if(nextcell == null) {
             runSucceed = false;
             throw new InvalidRunException();
         }
-        player.getCurrentCell().removePlayer(player);
         player.setCurrentCell(nextcell);
-        player.getCurrentCell().addPlayer(player);
         return runSucceed;
+    }
+    public boolean ActionRunToCell(int CellID) throws InvalidRunException {
+        for(Cell c : player.getCurrentCell().getAdjacentCells()){
+            if(c.getCellID() == CellID) {
+                player.setCurrentCell(c);
+                return true;
+            }
+        }
+        throw new InvalidRunException();
     }
 }
